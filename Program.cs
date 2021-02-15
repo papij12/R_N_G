d3b0206cc6afcd6e6cde531491adc10798386212
irene iradukunda <iradukunda.irene200@gmail.com>
2021-02-15 16:09:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace R_N_G
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(11, 20));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
